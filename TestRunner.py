63582cbfdc6aa59cb411c99cb80db1ff622dec1a
MeshCollider <dobsonsa68@gmail.com>
2017-09-21 20:30:44 +1200

    Testing os.system

--- TestRunner.py
+++ TestRunner.py
@@ -1,19 +1,24 @@
 import zipfile
 import os
+import shutil
 
 
-MAVEN_DIR = "Maven Test Sources/"
+MAVEN_TEST_DIR = "Maven Test Sources/"
+MAVEN_SOURCE_DIR = "../mvn-data/"
 
-for rootdir,dirs,files in os.walk(MAVEN_DIR):
+
+for rootdir,dirs,files in os.walk(MAVEN_TEST_DIR):
     for filename in files:
         if filename.endswith(".jar"):
             fullPath = os.path.join(rootdir,filename)
             outputDir = os.path.join(rootdir,filename[:-4])
-            print(outputDir)
+            # print(outputDir)
             if not os.path.exists(outputDir):
                 os.makedirs(outputDir)
-            else:
-                continue
-            zip_ref = zipfile.ZipFile(fullPath, 'r')
-            zip_ref.extractall(outputDir)
-            zip_ref.close()
+                zip_ref = zipfile.ZipFile(fullPath, 'r')
+                zip_ref.extractall(outputDir)
+                zip_ref.close()
+            # move the sources into the correct directory structure
+            os.system("pwd")
+
+            
